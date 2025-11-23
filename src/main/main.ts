e929f4a19fcb140f711c8eb5c09840576a5f5613
konghdlddev <konghdld.dev@gmail.com>
2025-11-23 18:33:18 +0700

    not border

--- src/main/main.ts
+++ src/main/main.ts
@@ -98,8 +98,8 @@ async function generateImageWithPadding(base64: string, paddingPercent = 0): Pro
         align-items: center;
       }
       img {
-        max-width: calc(100% + ${12}%);
-        max-height: calc(100% + ${12}%);
+        max-width: calc(100% + ${14}%);
+        max-height: calc(100% + ${14}%);
         width: auto;
         height: auto;
         object-fit: contain;
