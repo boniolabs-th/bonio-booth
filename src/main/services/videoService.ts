0786dd9e35cfcbd5a19ddcd0a376c36065554cb7
myproject <70787902+mrslomo@users.noreply.github.com>
2026-01-29 00:45:49 +0700

    Update videoService.ts

--- src/main/services/videoService.ts
+++ src/main/services/videoService.ts
@@ -54,18 +54,19 @@ export const createBoomerangVideo = async (
       // ยุบเหลือชุดเดียว: ลับคมนิดหน่อย (unsharp) + รวมวิดีโอ (concat) + ย่อเป็น 720p เพื่อความลื่น by all
       '[0:v]fps=30,unsharp=3:3:0.8,reverse,fifo[r];' +
       '[0:v]fps=30,unsharp=3:3:0.8[o];' +
-      '[o][r]concat=n=2:v=1:a=0,scale=720:-2,setsar=1',
+      '[o][r]concat=n=2:v=1:a=0,scale=1080:-2,setsar=1',
       '-c:v',
       'libx264',
       '-preset',
-      'ultrafast', //tune by all increase compression.
+      'veryfast', //tune by all increase compression.
       '-crf',
-      '20', // ปรับ 20 เป็น 23
+      '24', // ปรับ 20 เป็น 24
       '-r',
       '30',
       '-pix_fmt',
       'yuv420p',
       '-movflags',
+      '-an',                 // cut sound by all
       '+faststart',
       '-y',
       output,
@@ -124,7 +125,7 @@ export const createBoomerangGif = async (
       '-i',
       inputVideoPath,
       '-filter_complex',
-      '[0:v]fps=15,reverse,fifo[r];[0:v]fps=15[o];[o][r]concat=n=2:v=1:a=0,scale=720:-2:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=256[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3',
+      '[0:v]fps=15,reverse,fifo[r];[0:v]fps=15[o];[o][r]concat=n=2:v=1:a=0,scale=1080:-2:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=256[p];[s1][p]paletteuse=dither=bayer:bayer_scale=3',
       '-loop',
       '0', // Infinite loop
       '-y',
@@ -189,7 +190,7 @@ export const extractFrames = async (
       '-i',
       inputVideoPath,
       '-vf',
-      `select='not(mod(n\\,${Math.floor(30 / frameCount)}))',unsharp=5:5:1.2:5:5:0.0,scale=720:-2:flags=lanczos`,
+      `select='not(mod(n\\,${Math.floor(30 / frameCount)}))',unsharp=5:5:1.2:5:5:0.0,scale=1080:-2:flags=lanczos`,
       '-vsync',
       'vfr',
       '-frames:v',
@@ -390,7 +391,7 @@ export const createBoomerangWithLut = async (
       '-i',
       inputVideoPath,
       '-filter_complex',
-      `[0:v]fps=30,reverse,fifo[r];[0:v]fps=30[o];[o][r]concat=n=2:v=1:a=0,scale=720:-2,setsar=1,lut3d=${lutFileName}[v]`,
+      `[0:v]fps=30,reverse,fifo[r];[0:v]fps=30[o];[o][r]concat=n=2:v=1:a=0,scale=1080:-2,setsar=1,lut3d=${lutFileName}[v]`,
       '-map',
       '[v]',
       '-c:v',
